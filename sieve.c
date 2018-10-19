ec2ca73ec481956867801c402bb07c515b7bff62
ibelkebeer <ibelkebir@stuy.edu>
2018-10-19 15:29:15 -0400

    finished base sieve

---
+++ sieve.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "sieve.h"
+
+int sieve(int targetPrime){
+  int len;
+  if(targetPrime > 5000){
+    len = 1.15 * targetPrime * log(targetPrime);
+  }else{
+    len = 1.3 * targetPrime * log(targetPrime) + 10;
+  }
+  int* cur = calloc(len, sizeof(int));
+  int i;
+  int index = 2;
+  for(i = 1; i <= targetPrime; i++){
+    while(cur[index]){
+      index++;
+    }
+    if(i == targetPrime){
+      return index;
+    }
+    int j;
+    for(j = 2 * index; j < len; j += index){
+      cur[j] = 1;
+    }
+    index++;
+  }
+}
+
