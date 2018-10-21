f543f7f202fbfe1a95442ecc153e9107074eb26a
Imad <ibelkebir@stuy.edu>
2018-10-21 01:25:05 -0400

    but faster

--- sieve.c
+++ sieve.c
@@ -64,34 +64,21 @@ int fastsieve(int targetPrime){
   if(targetPrime > 5000){
     len = 1.31 * targetPrime * log(targetPrime);
   }else{
-    len = 1.3 * targetPrime * log(targetPrime) + 10;
+    len = 1.5 * targetPrime * log(targetPrime) + 10;
   }
-  int* cur = calloc(len, sizeof(int));
+  int* cur = calloc((len * 2) / 192, sizeof(int));
   int mult3 = 1;
   long index = 5;
   int i = 3;
   int indexer;
   long j;
-  while(i <= targetPrime){
-    indexer = ((index - (((index+1) / 3) - 1)) >> 1) & 31;
-    if(i == targetPrime){
-      while(cur[(index*2) / 192] & (1 << indexer)){
-        mult3++;
-        mult3 %= 3;
-        if(mult3){
-          index += 2;
-        }else{
-          index += 4;
-          mult3++;
-        }
-        indexer = ((index - (((index+1) / 3) - 1)) >> 1) & 31;
-      }
-      return index;
-    }
+  while(i - 3 < (int)sqrt(len) + 1){
+    //printf("%d\n%d\n\n", i, index);
+    indexer = ((index - (((index+1) / 3) - 2)) >> 1) & 31;
     if(!(cur[(index*2) / 192] & (1 << indexer))){
       for(j = index * index; j < len; j += 2 * index){
         if(j % 3){
-          indexer = ((j - (((j+1) / 3) - 1)) >> 1) & 31;
+          indexer = ((j - (((j+1) / 3) - 2)) >> 1) & 31;
           cur[(j * 2) / 192] |= (1 << indexer);
         }
       }
@@ -104,6 +91,47 @@ int fastsieve(int targetPrime){
       mult3++;
     }else{
       index += 2;
+    }
+  }
+  while(i <= targetPrime){
+    indexer = ((index - (((index+1) / 3) - 2)) >> 1) & 31;
+    while(cur[(index*2) / 192] & (1 << indexer)){
+      mult3++;
+      mult3 %= 3;
+      if(mult3){
+        index += 2;
+      }else{
+        index += 4;
+        mult3++;
+      }
+      indexer = ((index - (((index+1) / 3) - 2)) >> 1) & 31;
+    }
+    if(i < targetPrime){
+      mult3++;
+      mult3 %= 3;
+      if(mult3){
+        index += 2;
+      }else{
+        index += 4;
+        mult3++;
+      }
+      indexer = ((index - (((index+1) / 3) - 2)) >> 1) & 31;
+      while(cur[(index*2) / 192] & (1 << indexer)){
+        mult3++;
+        mult3 %= 3;
+        if(mult3){
+          index += 2;
+        }else{
+          index += 4;
+          mult3++;
+        }
+        indexer = ((index - (((index+1) / 3) - 2)) >> 1) & 31;
+      }
+      i++;
+    }
+    //printf("%d\n%d\n\n", i, index);
+    if(i == targetPrime){
+      return index;
     }
   }
   return 0;
